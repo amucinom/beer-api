8a304757376c59d859dc2c9198d079dbdd3d676c
Aldo Mucino <amucinom@gmail.com>
2016-05-30 00:59:23 -0700

    fixed spacing

--- client/app/add/add.controller.js
+++ client/app/add/add.controller.js
@@ -1,6 +1,7 @@
 angular.module('beerApp')
-	.controller('addCtrl', ['$scope', '$rootScope','Beer', '$location', function($scope, $rootScope,Beer, $location) {
+	.controller('addCtrl', ['$scope', '$rootScope', 'Beer', '$location',
+		function($scope, $rootScope, Beer, $location) {
 
-		$rootScope.PAGE = 'new';
+			$rootScope.PAGE = 'new';
 
-	}]);
+		}]);
